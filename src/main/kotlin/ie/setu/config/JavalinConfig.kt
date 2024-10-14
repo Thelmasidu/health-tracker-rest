844211c89dc4f89b81b54f31d64e2964f4299feb
Thelmasidu <20109631@mail.wit.ie>
2024-10-14 20:55:11 +0100

    Add routes for user management in Javalin configuration
    Updated the JavalinConfig class to register routes for user management operations in the Health Tracker API:
    
    GET /api/users: Fetch all users.
    GET /api/users/{user-id}: Retrieve a user by ID.
    POST /api/users: Add a new user.
    GET /api/users/email/{user-email}: Fetch a user by email.
    DELETE /api/users/{user-id}: Delete a user by ID.
    PATCH /api/users/{user-id}: Update a user by ID.
    These routes enable CRUD operations for users, improving the API's functionality for managing user data.

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -20,6 +20,10 @@ class JavalinConfig {
     private fun registerRoutes(app: Javalin) {
         app.get("/api/users", HealthTrackerController::getAllUsers)
         app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
+        app.post("/api/users", HealthTrackerController::addUser)
+        app.get("/api/users/email/{user-email}", HealthTrackerController::getUserByEmail)
+        app.delete("/api/users/{user-id}", HealthTrackerController::deleteUser)
+        app.patch("/api/users/{user-id}", HealthTrackerController::updateUser)
     }
 
 }
