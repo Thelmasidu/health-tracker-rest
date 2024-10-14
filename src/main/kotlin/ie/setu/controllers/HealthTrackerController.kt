60c564f1fdec9d56802f0aebb92168c98d70a341
Thelmasidu <20109631@mail.wit.ie>
2024-10-14 20:55:44 +0100

    Refactor HealthTrackerController to enhance user management
    Enhanced the HealthTrackerController with the following updates:
    
    getUserByEmail(ctx: Context): Improved the method to return user data for a given email, including proper error handling for non-existent users.
    addUser(ctx: Context): Adjusted the method to read the user data from the request body using Jackson's ObjectMapper, followed by saving the user and returning the created user in the response.
    deleteUser(ctx: Context): Enhanced the deletion method to return the deleted user or a 404 status if not found.
    updateUser(ctx: Context): Added functionality to update user details, ensuring that the response returns the updated user or a 404 status if the user is not found.
    These changes streamline the user management process and improve the overall error handling in the controller, ensuring a more robust API.

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -2,6 +2,9 @@ package ie.setu.controllers
 
 import ie.setu.domain.repository.UserDAO
 import io.javalin.http.Context
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import ie.setu.domain.User
 
 object HealthTrackerController {
 
@@ -18,6 +21,41 @@ object HealthTrackerController {
         }
     }
 
+    fun getUserByEmail(ctx: Context){
+        val email = userDao.findByEmail(ctx.pathParam("user-email"))
+        if (email != null) {
+            ctx.json(email)
+        }
+    }
+
+    fun addUser(ctx: Context) {
+        val mapper = jacksonObjectMapper()
+        val user = mapper.readValue<User>(ctx.body())
+        userDao.save(user)
+        ctx.json(user)
+    }
+
+    fun deleteUser(ctx: Context){
+        val user = userDao.delete(ctx.pathParam("user-id").toInt())
+        if (user != null) {
+            ctx.json(user)
+        } else {
+            ctx.status(404).result("User not found")
+        }
+    }
+
+    fun updateUser(ctx: Context) {
+        val mapper = jacksonObjectMapper()
+        val updatedUser = mapper.readValue<User>(ctx.body())
+
+        val user = userDao.update(ctx.pathParam("user-id").toInt(), updatedUser)
+
+        if (user != null) {
+            ctx.json(user)
+        } else {
+            ctx.status(404).result("User not found")
+        }
+    }
 
 }
 
