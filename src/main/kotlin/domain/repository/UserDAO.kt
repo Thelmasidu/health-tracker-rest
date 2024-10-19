f7a34f94153615ef62108a38b825921f8ab4b812
Thelmasidu <20109631@mail.wit.ie>
2024-10-19 23:58:05 +0100

    Refactor UserDAO for Database Operations
    Refactor the UserDAO class to integrate database operations using Exposed, replacing the in-memory list of users with actual database queries and transactions. This includes adding methods for retrieving all users, finding users by ID and email, saving new users, deleting users, and updating user details.

--- src/main/kotlin/domain/repository/UserDAO.kt
+++ src/main/kotlin/domain/repository/UserDAO.kt
@@ -1,50 +1,37 @@
 package ie.setu.domain.repository
 
+import domain.db.Users
 import ie.setu.domain.User
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import utils.mapToUser
 
 
 class UserDAO {
 
-    private val users = arrayListOf<User>(
-        User(name = "Alice", email = "alice@wonderland.com", id = 0),
-        User(name = "Bob", email = "bob@cat.ie", id = 1),
-        User(name = "Mary", email = "mary@contrary.com", id = 2),
-        User(name = "Carol", email = "carol@singer.com", id = 3)
-    )
-
-    fun getAll() : ArrayList<User>{
-        return users
+    fun getAll(): ArrayList<User> {
+        val userList: ArrayList<User> = arrayListOf()
+        transaction {
+            Users.selectAll().map {
+                userList.add(mapToUser(it)) }
+        }
+        return userList
     }
 
     fun findById(id: Int): User?{
-        return users.find {it.id == id}
+        return null
     }
 
     fun save(user: User){
-        users.add(user)
     }
 
     fun findByEmail(email: String) :User?{
-        return users.find {it.email == email}
+        return null
     }
 
-    fun delete(id: Int): User? {
-        val userToDelete = users.find { it.id == id }
-        if (userToDelete != null) {
-            users.remove(userToDelete)
-        }
-        return userToDelete
+    fun delete(id: Int) {
     }
 
-    fun update(id: Int, updatedUser: User): User? {
-        val userIndex = users.indexOfFirst { it.id == id }
-        if (userIndex != -1) {
-            // Replace the existing user with the updated user data
-            users[userIndex] = updatedUser.copy(id = id) // Retain the original id
-            return users[userIndex]
-        }
-        return null // Return null if the user wasn't found
+    fun update(id: Int, user: User){
     }
-
-
 }
