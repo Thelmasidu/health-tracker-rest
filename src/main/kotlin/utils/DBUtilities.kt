cc3b061aeffb5b604371f2db5c33104e03ec8a8c
Thelmasidu <20109631@mail.wit.ie>
2024-10-19 23:59:47 +0100

    Add User Mapping Utility
    Create a utility function to map ResultRow objects to User domain objects, facilitating the conversion of database query results into usable application entities.

---
+++ src/main/kotlin/utils/DBUtilities.kt
@@ -0,0 +1,11 @@
+package utils
+
+import domain.db.Users
+import ie.setu.domain.User
+import org.jetbrains.exposed.sql.ResultRow
+
+fun mapToUser(it: ResultRow) = User(
+    id = it[Users.id],
+    name = it[Users.name],
+    email = it[Users.email]
+)
