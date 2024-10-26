ac9671af9b2856522ada39d2d0be099f3fadabad
Thelmasidu <20109631@mail.wit.ie>
2024-10-26 22:13:54 +0100

    Add activity handling functions in HealthTrackerController
    Implemented functions in HealthTrackerController for managing activities:
    getAllActivities: Retrieves all activities, serializing Joda date to JSON.
    getActivitiesByUserId: Retrieves activities by user ID, with JSON date serialization.
    addActivity: Adds a new activity, deserializing JSON to Activity object.
    Configured Jackson object mapper with JodaModule for date handling.

--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -1,14 +1,19 @@
 package ie.setu.controllers
 
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.joda.JodaModule
 import ie.setu.domain.repository.UserDAO
 import io.javalin.http.Context
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import domain.Activity
 import ie.setu.domain.User
+import ie.setu.repository.ActivityDAO
 
 object HealthTrackerController {
 
     private val userDao = UserDAO()
+    private val activityDAO = ActivityDAO()
 
     fun getAllUsers(ctx: Context) {
         ctx.json(userDao.getAll())
@@ -56,6 +61,42 @@ object HealthTrackerController {
             ctx.status(404).result("User not found")
         }
     }
+
+    //--------------------------------------------------------------
+    // ActivityDAO specifics
+    //-------------------------------------------------------------
+
+    fun getAllActivities(ctx: Context) {
+        //mapper handles the deserialization of Joda date into a String.
+        val mapper = jacksonObjectMapper()
+            .registerModule(JodaModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        ctx.json(mapper.writeValueAsString( activityDAO.getAll() ))
+    }
+
+    fun getActivitiesByUserId(ctx: Context) {
+        if (userDao.findById(ctx.pathParam("user-id").toInt()) != null) {
+            val activities = activityDAO.findByUserId(ctx.pathParam("user-id").toInt())
+            if (activities.isNotEmpty()) {
+                //mapper handles the deserialization of Joda date into a String.
+                val mapper = jacksonObjectMapper()
+                    .registerModule(JodaModule())
+                    .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                ctx.json(mapper.writeValueAsString(activities))
+            }
+        }
+    }
+
+    fun addActivity(ctx: Context) {
+        //mapper handles the serialisation of Joda date into a String.
+        val mapper = jacksonObjectMapper()
+            .registerModule(JodaModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+        val activity = mapper.readValue<Activity>(ctx.body())
+        activityDAO.save(activity)
+        ctx.json(activity)
+    }
+
 
 }
 
