667844a1994326f10cf40de2eb1e5fe3a4c23778
Thelmasidu <20109631@mail.wit.ie>
2024-10-26 22:23:15 +0100

    Add mapping utilities for User and Activity entities
    Implemented mapToUser and mapToActivity functions to convert database ResultRow objects into User and Activity data classes.
    Enhances readability and maintainability of code when interacting with the database.

--- src/main/kotlin/utils/DBUtilities.kt
+++ src/main/kotlin/utils/DBUtilities.kt
@@ -1,5 +1,7 @@
 package utils
 
+import domain.Activity
+import domain.db.Activities
 import domain.db.Users
 import ie.setu.domain.User
 import org.jetbrains.exposed.sql.ResultRow
@@ -8,4 +10,13 @@ fun mapToUser(it: ResultRow) = User(
     id = it[Users.id],
     name = it[Users.name],
     email = it[Users.email]
+)
+
+fun mapToActivity(it: ResultRow) = Activity(
+    id = it[Activities.id],
+    description = it[Activities.description],
+    duration = it[Activities.duration],
+    started = it[Activities.started],
+    calories = it[Activities.calories],
+    userId = it[Activities.userId]
 )
