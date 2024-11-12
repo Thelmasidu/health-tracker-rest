82586e896ba9c0f041797733e72c7f2950afe047
Thelmasidu <20109631@mail.wit.ie>
2024-11-12 14:16:00 +0000

    Add mapToHealthHistory Function for Mapping HealthHistories Table Rows
    - Implemented `mapToHealthHistory` function to map database rows from the `HealthHistories` table to `HealthHistory` data class instances.
    - Mapping includes fields for heart rate, cholesterol levels, blood sugar levels, weight, height, date of record, blood pressure, and user ID.
    - This function enhances data transformation consistency between the database and the application layer.

--- src/main/kotlin/utils/DBUtilities.kt
+++ src/main/kotlin/utils/DBUtilities.kt
@@ -3,7 +3,9 @@ package utils
 import domain.Activity
 import domain.db.Activities
 import domain.db.Users
+import ie.setu.domain.HealthHistory
 import ie.setu.domain.User
+import ie.setu.domain.db.HealthHistories
 import org.jetbrains.exposed.sql.ResultRow
 
 fun mapToUser(it: ResultRow) = User(
@@ -19,4 +21,15 @@ fun mapToActivity(it: ResultRow) = Activity(
     started = it[Activities.started],
     calories = it[Activities.calories],
     userId = it[Activities.userId]
+)
+fun mapToHealthHistory(it: ResultRow) = HealthHistory(
+    id = it[HealthHistories.id],
+    heartRate = it[HealthHistories.heartRate],
+    cholesterolLevels = it[HealthHistories.cholesterolLevels],
+    bloodSugarLevels = it[HealthHistories.bloodSugarLevels],
+    weight = it[HealthHistories.weight],
+    height = it[HealthHistories.height],
+    dateOfRecord = it[HealthHistories.dateOfRecord],
+    bloodPressure = it[HealthHistories.bloodPressure],
+    userId = it[HealthHistories.userId]
 )
