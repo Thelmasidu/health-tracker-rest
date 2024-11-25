a8fdfd4a4d4d3d1e63df3ac5d29830f4293c2321
Thelmasidu <20109631@mail.wit.ie>
2024-11-25 13:43:17 +0000

    Add a medication overview component to display the medication list.
    - Added the'medication-overview' component for listing medication records.
    - Retrieves medication data from the API and shows the medicine's name.
    
    - Uses Axios to handle API queries, including error handling for failed retrieves.

---
+++ src/main/resources/vue/views/medication-overview.vue
@@ -0,0 +1,32 @@
+<template id="medication-overview">
+  <app-layout>
+    <div>
+      <div>
+        <ul class="medication-overview-list">
+          <li v-for="medication in medication">
+            <a :href="`/medication/${medication.id}`">{{medication.medicationName}} </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </app-layout>
+
+</template>
+<script>
+app.component("medication-overview", {
+  template: "#medication-overview",
+  data: () => ({
+    medication: [],
+  }),
+  created() {
+    this.fetchMedications();
+  },
+  methods: {
+    fetchMedications: function () {
+      axios.get("/api/medication")
+          .then(res => this.medication = res.data)
+          .catch(() => alert("Error while fetching medication"));
+    }
+  }
+});
+</script>
